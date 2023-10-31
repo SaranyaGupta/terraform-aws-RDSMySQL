9d680115e79674a84d187f546f85a0365be85cdf
Saranya Gupta <saranya.gupta@coforge.com>
2023-10-31 22:34:57 +0530

    added option group and parameter group

--- main.tf
+++ main.tf
@@ -12,3 +12,28 @@ module "db_subnet_group" {
 
   tags = merge(var.tags, var.db_subnet_group_tags)
 }
+module "db_parameter_group" {
+  source          = "modules/db_parameter_group"
+  create          = local.create_db_parameter_group
+  parameter_name            = var.parameter_group_name
+  parameter_name_prefix = var.parameter_group_use_name_prefix
+  description     = var.parameter_group_description
+  family          = var.family
+  parameters      = var.parameters
+
+  tags = merge(var.tags, var.db_parameter_group_tags)
+}
+
+module "db_option_group" {
+  source                   = "modules/db_option_group"
+  create                   = local.create_db_option_group  
+  option_name              = var.option_group_name
+  option_name_prefix       = var.option_group_use_name_prefix
+  description              = var.option_group_description
+  engine_name              = var.engine
+  major_engine_version     = var.major_engine_version
+  options                  = var.options
+  timeouts                 = var.option_group_timeouts
+
+  tags = merge(var.tags, var.db_option_group_tags)
+}
--- variable.tf
+++ variable.tf
@@ -39,3 +39,92 @@ variable "db_subnet_group_tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "db_option_group_tags" {
+  description = "Additional tags for the DB option group"
+  type        = map(string)
+  default     = {}
+}
+
+variable "db_parameter_group_tags" {
+  description = "Additional tags for the  DB parameter group"
+  type        = map(string)
+  default     = {}
+}
+# DB option group
+variable "create_db_option_group" {
+  description = "Create a database option group"
+  type        = bool
+  default     = true
+}
+
+variable "option_group_name" {
+  description = "Name of the option group"
+  type        = string
+  default     = null
+}
+
+variable "option_group_use_name_prefix" {
+  description = "Determines whether to use `option_group_name` as is or create a unique name beginning with the `option_group_name` as the prefix"
+  type        = bool
+  default     = true
+}
+
+variable "option_group_description" {
+  description = "The description of the option group"
+  type        = string
+  default     = null
+}
+
+variable "major_engine_version" {
+  description = "Specifies the major version of the engine that this option group should be associated with"
+  type        = string
+  default     = null
+}
+
+variable "options" {
+  description = "A list of Options to apply"
+  type        = any
+  default     = []
+}
+variable "option_group_timeouts" {
+  description = "Define maximum timeout for deletion of `aws_db_option_group` resource"
+  type        = map(string)
+  default     = {}
+}
+# DB parameter group
+variable "create_db_parameter_group" {
+  description = "Whether to create a database parameter group"
+  type        = bool
+  default     = true
+}
+
+variable "parameter_group_name" {
+  description = "Name of the DB parameter group to associate or create"
+  type        = string
+  default     = null
+}
+
+variable "parameter_group_use_name_prefix" {
+  description = "Determines whether to use `parameter_group_name` as is or create a unique name beginning with the `parameter_group_name` as the prefix"
+  type        = bool
+  default     = true
+}
+
+variable "parameter_group_description" {
+  description = "Description of the DB parameter group to create"
+  type        = string
+  default     = null
+}
+
+variable "family" {
+  description = "The family of the DB parameter group"
+  type        = string
+  default     = null
+}
+
+variable "parameters" {
+  description = "A list of DB parameters (map) to apply"
+  type        = list(map(string))
+  default     = []
+}
