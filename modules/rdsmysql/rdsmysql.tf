f18b7dfcb31d87b5ffc617bf06fd085c3b095b21
Saranya Gupta <saranya.gupta@coforge.com>
2023-11-03 12:05:05 +0530

    changed

--- modules/rdsmysql/rdsmysql.tf
+++ modules/rdsmysql/rdsmysql.tf
@@ -1,6 +1,6 @@
 locals {
   monitoring_role_arn = var.create_monitoring_role ? aws_iam_role.enhanced_monitoring[0].arn : var.monitoring_role_arn
-  final_snapshot_identifier = var.skip_final_snapshot ? null : "${var.final_snapshot_identifier_prefix}-${var.identifier}-${try(random_id.snapshot_identifier[0].hex, "")}"
+  #final_snapshot_identifier = var.skip_final_snapshot ? null : "${var.final_snapshot_identifier_prefix}-${var.identifier}-${try(random_id.snapshot_identifier[0].hex, "")}"
   identifier        = var.use_identifier_prefix ? null : var.identifier
   identifier_prefix = var.use_identifier_prefix ? "${var.identifier}-" : null
   monitoring_role_name        = var.monitoring_role_use_name_prefix ? null : var.monitoring_role_name
@@ -65,7 +65,7 @@ resource "aws_db_instance" "rdsmysql" {
   snapshot_identifier       = var.snapshot_identifier
   copy_tags_to_snapshot     = var.copy_tags_to_snapshot
   skip_final_snapshot       = var.skip_final_snapshot
-  final_snapshot_identifier = local.final_snapshot_identifier
+  #final_snapshot_identifier = var.final_snapshot_identifier
 
   performance_insights_enabled          = var.performance_insights_enabled
   performance_insights_retention_period = var.performance_insights_enabled ? var.performance_insights_retention_period : null
