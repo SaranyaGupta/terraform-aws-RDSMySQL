6aa0cb483fb927a691d1b314e92d476f83597017
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-11-16 16:30:30 +0530

    Update outputs.tf

--- modules/db_parameter_group/outputs.tf
+++ modules/db_parameter_group/outputs.tf
@@ -3,6 +3,12 @@ output "db_parameter_group_id" {
   value       = try(aws_db_parameter_group.parameter_group[*].id, null)
 }
 
+output "db_parameter_group_name" {
+  description = "The db parameter group name"
+  value       = try(aws_db_parameter_group.parameter_group[*].name, null)
+}
+
+
 output "db_parameter_group_arn" {
   description = "The ARN of the db parameter group"
   value       = try(aws_db_parameter_group.parameter_group[*].arn, null)
