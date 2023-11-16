3576068bf37911d0f79451ae33d600c25d3431b9
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-11-16 16:29:52 +0530

    Update outputs.tf

--- modules/db_option_group/outputs.tf
+++ modules/db_option_group/outputs.tf
@@ -2,6 +2,10 @@ output "db_option_group_id" {
   description = "The db option group id"
   value       = try(aws_db_option_group.option_group[*].id, null)
 }
+output "db_option_group_name" {
+  description = "The db option group id"
+  value       = try(aws_db_option_group.option_group[*].name, null)
+}
 
 output "db_option_group_arn" {
   description = "The ARN of the db option group"
