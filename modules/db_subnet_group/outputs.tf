ef0ca5eed970dd697ece1922a359c84ff5d2c145
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-11-16 13:08:51 +0530

    Update outputs.tf

--- modules/db_subnet_group/outputs.tf
+++ modules/db_subnet_group/outputs.tf
@@ -2,7 +2,10 @@ output "db_subnet_group_id" {
   description = "The db subnet group name"
   value       = try(aws_db_subnet_group.subnet_groups[*].id, null)
 }
-
+output "db_subnet_group_name" {
+  description = "The db subnet group name"
+  value       = try(aws_db_subnet_group.name[*].id, null)
+}
 output "db_subnet_group_arn" {
   description = "The ARN of the db subnet group"
   value       = try(aws_db_subnet_group.subnet_groups[*].arn, null)
