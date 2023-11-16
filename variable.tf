1fe96460d7aeb0a59e5a119d965127fdb8855647
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-11-16 23:30:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,6 +10,18 @@ variable "security_rules" {
       })))
     }
 
+variable "existing_sg_rules" {
+
+      type = map(map(object({
+        type        = string
+        description = string
+        from_port   = number
+        to_port     = number
+        protocol    = string
+        cidr_blocks = list(string)
+      })))
+    }
+
 variable "identifier" {
   description = "The name of the RDS instance"
   type        = string
