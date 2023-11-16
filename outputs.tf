567dc28e69ecdc34153d713f1df5c4dfe633ef9d
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-11-16 13:09:24 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -103,7 +103,10 @@ output "db_subnet_group_id" {
   description = "The db subnet group name"
   value       = module.db_subnet_group.db_subnet_group_id
 }
-
+output "db_subnet_group_name" {
+  description = "The db subnet group name"
+  value       = module.db_subnet_group.db_subnet_group_name
+}
 output "db_subnet_group_arn" {
   description = "The ARN of the db subnet group"
   value       = module.db_subnet_group.db_subnet_group_arn
