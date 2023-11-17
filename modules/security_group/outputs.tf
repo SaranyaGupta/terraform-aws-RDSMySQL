06adc20e5812df05e4cd5036c795f973e36d0228
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-11-17 12:53:13 +0530

    Update outputs.tf

--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -1,3 +1,3 @@
 output "id" {
-  value = values(rds_security_groups in aws_security_group.rds_security_groups.[each.key.name]).id
+  value = values(aws_security_group.rds_security_groups.[each.key.name]).id
 }    
