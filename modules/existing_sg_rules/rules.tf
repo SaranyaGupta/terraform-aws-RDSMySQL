09a69663c8d919c5484397c2a03ee99a4c499505
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-11-17 14:34:25 +0530

    Update rules.tf

--- modules/existing_sg_rules/rules.tf
+++ modules/existing_sg_rules/rules.tf
@@ -17,6 +17,6 @@ resource "aws_security_group_rule" "rules" {
   protocol          = each.value.protocol
   cidr_blocks       = each.value.cidr_blocks
   description       = each.value.description
-  security_group_id = [each.value.name].id
+  security_group_id = name[each.value].id
   }
   
