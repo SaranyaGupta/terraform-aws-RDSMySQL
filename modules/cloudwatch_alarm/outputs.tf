3c42ca73e55a5afeb162a7a813b063231bcacdef
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-11-22 13:52:30 +0530

    Update outputs.tf

--- modules/cloudwatch_alarm/outputs.tf
+++ modules/cloudwatch_alarm/outputs.tf
@@ -1,4 +1,12 @@
-output "alarm_name" {
-  description = "The alarm name"
+output "alarm_name1" {
+  description = "The alarm name : CPU Utilization"
   value       = try(aws_cloudwatch_metric_alarm.cpu_utilization_too_high[*].alarm_name, null)
+}
+output "alarm_name2" {
+  description = "The alarm name : Memory Utilization"
+  value       = try(aws_cloudwatch_metric_alarm.memory_too_low[*].alarm_name, null)
+}
+output "alarm_name3" {
+  description = "The alarm name : Storage Utilization"
+  value       = try(aws_cloudwatch_metric_alarm.storage_space_too_low[*].alarm_name, null)
 }
