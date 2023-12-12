424107349911f34d5fe98b28ba05671d3f328aa4
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-12-12 11:41:21 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,1 +1,9 @@
-
+locals {
+  create_db_subnet_group    = var.create_db_subnet_group
+  create_db_parameter_group = var.create_db_parameter_group
+  create_db_instance        = var.create_db_instance
+  create_db_option_group = var.create_db_option_group
+  create_high_cpu_alarm = var.create_high_cpu_alarm
+  create_storage_space_too_low_alarm = var.create_storage_space_too_low_alarm
+  create_memory_too_low_alarm = var.create_memory_too_low_alarm
+  }
