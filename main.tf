99a1ade1ebbd9c5619bcbc24ca7f229dc0af3944
SaranyaGupta <106384097+SaranyaGupta@users.noreply.github.com>
2023-12-12 11:40:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-provider "aws" {
-  region = "us-east-2"
-}
-locals {
-  create_db_subnet_group    = var.create_db_subnet_group
-  create_db_parameter_group = var.create_db_parameter_group
-  create_db_instance        = var.create_db_instance
-  create_db_option_group = var.create_db_option_group
-  create_high_cpu_alarm = var.create_high_cpu_alarm
-  create_storage_space_too_low_alarm = var.create_storage_space_too_low_alarm
-  create_memory_too_low_alarm = var.create_memory_too_low_alarm
-  }
-
 module "db_subnet_group" {
   source          = "./modules/db_subnet_group"
   create          = local.create_db_subnet_group
